81bf30dece431f97b4b9e387bea0fcad78354177
Andy <andysterks@users.noreply.github.com>
2018-10-03 22:42:56 -0500

    Updates

--- ifElse_vs_function.js
+++ ifElse_vs_function.js
@@ -4,11 +4,12 @@ if (somethingIsTrue) {
 	number--;
 }
 
-
+// function that returns an incremented number
 function(number) {
 	return number + 1;
 }
 
+// function that increments a number and then exits using a return
 function(number) {
 	number++;
 	return;
