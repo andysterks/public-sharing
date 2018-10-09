3978db4e6287bc0342dd33b64da039a6a8497807
Andy <andysterks@users.noreply.github.com>
2018-10-09 14:53:00 -0500

    Create zero-variable.js

---
+++ zero-variable.js
@@ -0,0 +1,9 @@
+var someVariable = 0;
+
+var todaysDate = new Date();
+
+if (todaysDate.getFullYear() > 2018) {
+    someVariable = 1;
+}
+
+console.log(someVariable) // what will the output of this console.log be?
